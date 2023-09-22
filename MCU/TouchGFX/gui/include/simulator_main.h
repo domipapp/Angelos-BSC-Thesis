77b980d314cf06118b06e0e3de9b937b664ded94
Dominik Edvárd Papp <papp@edu.bme.hu>
2023-09-22 13:33:04 +0200

    Added main.h header for simulator.

---
+++ MCU/TouchGFX/gui/include/simulator_main.h
@@ -0,0 +1,24 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/* USER CODE BEGIN Private defines */
+#define IP_ADDRESS "192.168.1.249"	// Server IP Address
+#define PORT "9000"					// Server Port number
+// AT command for connecting to server
+#define WIFI_SSID "Telekom-2D6325"	// Local WIFI SSID
+#define WIFI_PASS "4njteenm6s7cx4cb"// Local WIFI password
+// AT command for connecting to wifi
+// Event flag
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
