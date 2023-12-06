682a3c2bd9c570f1a4d93b9ee754e4f803b4c6f3
domipapp <domi.papp55@gmail.com>
2023-12-06 09:21:14 +0100

    React fragment not needed, removed.

--- Frontend/angelos/src/components/common/myChart.jsx
+++ Frontend/angelos/src/components/common/myChart.jsx
@@ -57,52 +57,50 @@ class MyChart extends React.Component {
     const { data } = this.props;
     const { rangeSelectorData } = this.state;
     return (
-      <React.Fragment>
-        <div className="container">
-          <Chart
-            ref={this.chartRef}
-            id="zoomedChart"
-            palette="Harmony Light"
-            dataSource={data}>
-            <Series argumentField="date" valueField="value" />
-            <ArgumentAxis visualRange={this.state.visualRange}></ArgumentAxis>
-            <Legend visible={false} />
-            <CommonSeriesSettings>
-              <Point size={4} />
-            </CommonSeriesSettings>
-            <ValueAxis>
-              <Label format="decimal" />
-            </ValueAxis>
-            <Tooltip
-              enabled={true}
-              customizeTooltip={(pointInfo) => {
-                return {
-                  text: `Date: ${pointInfo.argumentText}\nValue: ${pointInfo.valueText}`,
-                };
-              }}
-            />
-          </Chart>
-          <RangeSelector
-            dataSource={rangeSelectorData}
-            onValueChanged={(e) => {
-              this.setState({ visualRange: e.value });
-            }}>
-            <Size height={80} />
-            <Margin left={10} />
-            <Scale
-              startValue={rangeSelectorData[0].date}
-              endValue={rangeSelectorData[rangeSelectorData.length - 1].date}
-              label={this.falseVisibility}
-            />
+      <div className="container">
+        <Chart
+          ref={this.chartRef}
+          id="zoomedChart"
+          palette="Harmony Light"
+          dataSource={data}>
+          <Series argumentField="date" valueField="value" />
+          <ArgumentAxis visualRange={this.state.visualRange}></ArgumentAxis>
+          <Legend visible={false} />
+          <CommonSeriesSettings>
+            <Point size={4} />
+          </CommonSeriesSettings>
+          <ValueAxis>
+            <Label format="decimal" />
+          </ValueAxis>
+          <Tooltip
+            enabled={true}
+            customizeTooltip={(pointInfo) => {
+              return {
+                text: `Date: ${pointInfo.argumentText}\nValue: ${pointInfo.valueText}`,
+              };
+            }}
+          />
+        </Chart>
+        <RangeSelector
+          dataSource={rangeSelectorData}
+          onValueChanged={(e) => {
+            this.setState({ visualRange: e.value });
+          }}>
+          <Size height={80} />
+          <Margin left={10} />
+          <Scale
+            startValue={rangeSelectorData[0].date}
+            endValue={rangeSelectorData[rangeSelectorData.length - 1].date}
+            label={this.falseVisibility}
+          />
 
-            <ChartOptions palette="Harmony Light">
-              <Behavior valueChangeMode="onHandleMove" />
-              <Legend visible={false} />
-              <Series argumentField="date" valueField="value" />
-            </ChartOptions>
-          </RangeSelector>
-        </div>
-      </React.Fragment>
+          <ChartOptions palette="Harmony Light">
+            <Behavior valueChangeMode="onHandleMove" />
+            <Legend visible={false} />
+            <Series argumentField="date" valueField="value" />
+          </ChartOptions>
+        </RangeSelector>
+      </div>
     );
   }
 }
