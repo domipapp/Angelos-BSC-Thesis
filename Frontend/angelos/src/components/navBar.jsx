47077a070063ab92a68c4dc9f5a10cdb8c967127
domipapp <domi.papp55@gmail.com>
2023-12-06 09:22:05 +0100

    Modified from class into SFC.

--- Frontend/angelos/src/components/navBar.jsx
+++ Frontend/angelos/src/components/navBar.jsx
@@ -1,56 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlassChart } from "@fortawesome/free-solid-svg-icons";
+const Navbar = ({ ids }) => {
+  return (
+    <nav
+      className="navbar navbar-expand  navbar-light"
+      style={{ backgroundColor: "#a7826ca2" }}>
+      <div className="container-fluid">
+        <FontAwesomeIcon icon={faMagnifyingGlassChart} flip="horizontal" />
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    const { ids } = this.props;
-
-    return (
-      <nav
-        className="navbar navbar-expand  navbar-light"
-        style={{ backgroundColor: "#a7826ca2" }}>
-        <div className="container-fluid">
-          <FontAwesomeIcon icon={faMagnifyingGlassChart} flip="horizontal" />
-
-          <Link className="navbar-brand" to="/">
-            Angelos
-          </Link>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    Show sensor
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown">
-                    {ids.map((id) => (
-                      <NavLink
-                        className="dropdown-item"
-                        to={`/sensor/${id}`}
-                        key={id}>
-                        {id}
-                      </NavLink>
-                    ))}
-                  </ul>
-                </li>
-              </ul>
-            </div>
+        <Link className="navbar-brand" to="/">
+          Angelos
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Show sensor
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  {ids.map((id) => (
+                    <NavLink
+                      className="dropdown-item"
+                      to={`/sensor/${id}`}
+                      key={id}>
+                      {id}
+                    </NavLink>
+                  ))}
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar;
+export default Navbar;
